242641fb28e4419a81d3ab5829dc5f933390adff
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-11-30 10:38:19 -0500

    switch

--- Board.rb
+++ Board.rb
@@ -1,6 +1,7 @@
 class Board
+    
     def initialize
-        @grid = Array.new(8) { Array.new(8,'_')}
+        @grid = Array.new(8) { Array.new(8, nil)}
         
     end
 
@@ -9,9 +10,26 @@ class Board
         @grid[row][col]
     end
 
-    def []=(val)
+    def []=(pos, val)
         row, col = pos
         @grid[row][col] = val
+    end
+
+    # def populate
+    #     @grid.each do |row|
+    #         row.map do |pos|
+    #             row[pos] = 'piece'
+
+    #         end
+    #     end
+    # end
+
+    def populate
+        @grid.each_with_index do |rows, i|
+            
+            rows.each { |place| place = "piece" }
+        end
+        @grid
     end
 
     def move_piece(start_pos, end_pos)
@@ -21,8 +39,3 @@ class Board
 
 end
 
-class Piece
-    def initialize
-        
-    end
-end
